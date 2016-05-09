f4f673055df7d14b8a2bb47585b9abfeb9498384
josecper <josecper@ucm.es>
2016-05-09 16:44:38 +0200

    qkjwkfnqbfljwk

--- bspline.py
+++ bspline.py
@@ -27,11 +27,20 @@ def condition_array(knot_points, times):
 
     return a
 
-def solve(times, data, knot_points, l=0, reg_degree=2):
+def solve(times, data, knot_points, l=0, reg_degree=2, spline_class="equal", degree=3, base=None, return_base = False):
 
     #get condition array
     n = len(knot_points)
-    a = condition_array(knot_points, times)
+
+    if base is None:
+        if spline_class == "equal":
+            a = condition_array(knot_points, times)
+        elif spline_class == "deboor":
+            a = deboor_base(knot_points, times, degree)
+        else:
+            raise Exception("it bad")
+    else:
+        a = base
 
     #create regularization array
     #d = -2*numpy.eye(n)+numpy.eye(n+1)[1:,:-1]+numpy.eye(n+1)[:-1,1:]
@@ -42,13 +51,15 @@ def solve(times, data, knot_points, l=0, reg_degree=2):
     at = a.transpose()
 
     #adda = numpy.matmul(at,numpy.matmul(dd,at))
-    #smash the it
-    spline_coefs = (numpy.linalg.inv(at@a+l*dd) @ at) @ data
+    spline_coefs = (numpy.linalg.inv(at @ a + l*dd) @ at) @ data
 
     #return pyplot.plot(times, numpy.matmul(a, spline_coefs), "k", times, data, "g+")
     #spline_coefs = numpy.matmul(numpy.matmul(numpy.linalg.inv(numpy.matmul(at, a)+l*adda),at),data)
 
-    return spline_coefs
+    if return_base:
+        return spline_coefs, a
+    else:
+        return spline_coefs
 
 def spline_rep(times, spline_coefs, knot_points, summing=True):
 
@@ -60,7 +71,7 @@ def spline_rep(times, spline_coefs, knot_points, summing=True):
     
     return y.sum(axis=1)
 
-def deboor_base(t,k,d):
+def deboor_base(k,t,d):
     
     tgrid, kgrid = numpy.meshgrid(t,k,indexing="ij")
     kgridp1 = numpy.concatenate((kgrid[:,1:],kgrid[:,-1:]),axis=1)
@@ -83,7 +94,10 @@ def deboor_base(t,k,d):
         dif_i[numpy.isinf(dif_i)]=0
         dif_im1[numpy.isinf(dif_im1)]=0
         
-        y = dif_i*deboor_base(t,k,d-1) + dif_im1*deboor_base(t,k_p1,d-1)
+        y = dif_i*deboor_base(k,t,d-1) + dif_im1*deboor_base(k_p1,t,d-1)
+
+        #quick
+        y[-1,:-d] = y[0, -(d+1)::-1]
     
-    #y[numpy.isnan(y)]=0
     return y
+
